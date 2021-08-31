41e4a15d0a09b84107ab20034732130fe271494d
Dimension <to.hitesh.gorana@gmail.com>
2021-08-31 15:27:34 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -3,14 +3,11 @@ from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import streamlit as st
 SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 587
-USERNAME = st.secrets["username"]
-PASSWORD = st.secrets["password"]
 
 
-def send_email(filename, RECIEVER_EMAIL, bodyText):
+def send_email(filename, RECIEVER_EMAIL, bodyText, USERNAME, PASSWORD):
     subject = 'Backlink tool'
     message = MIMEMultipart()
     message["From"] = USERNAME
