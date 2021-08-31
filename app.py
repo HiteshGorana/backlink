ac8a7803e836e6ef919f621717e13b7c1c074e28
Dimension <to.hitesh.gorana@gmail.com>
2021-08-31 18:07:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -183,8 +183,8 @@ def main():
     if not file:
         st.error('File not Uploaded')
     st.write('Input Format')
-    st.write('BU : Brand URL (Service/Product) page')
-    st.write('AWU : Authority Website URL (Published Authority Post URL)')
+    st.write('**BU** : Brand URL (Service/Product) page')
+    st.write('**AWU** : Authority Website URL (Published Authority Post URL)')
     st.table(df.head(1))
     if submit_button and email != 'invalid':
         try:
